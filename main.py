75a5a6e43b74f31944fa14e366fa147eaa770595
João Victor Martins Reis <74378259+jvmreis@users.noreply.github.com>
2025-05-30 16:11:34 +0200

    LLM is workin now

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from dotenv import load_dotenv
 import os
+from langchain_openai import ChatOpenAI
 
 load_dotenv()  # carrega as variáveis do .env
 
-api_key = os.getenv("API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 debug = os.getenv("DEBUG")
 
 print(f"API Key: {api_key}")
 print(f"Debug mode: {debug}")
+
+llm = ChatOpenAI(
+    api_key=os.getenv('OPENAI_API_KEY'),
+    base_url="https://api.featherless.ai/v1",
+    model="THUDM/GLM-4-32B-0414",
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    (
+        "human",
+        "I love programming."
+    ),
+]
+ai_msg = llm.invoke(messages)
+print(ai_msg)
